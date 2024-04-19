99b3f6be56df67fb233f61ecd7be8e76bf8c17d2
Marko <markoyanovych@MacBook-Pro-Marko.local>
2024-04-19 12:14:46 +0300

    Complete Arrays and Cycles task

--- index.js
+++ index.js
@@ -1,43 +1,79 @@
-const car = {
-  color: 'black',
+// Task 1
+const movies = ['Inception', 'The Dark Knight', 'Interstellar', 'Iron Man', 'Pulp Fiction'];
+
+for (let movie of movies) {
+    console.log(movie);
 }
 
-car.color = 'green';
+// Task 2
+const carManufacturers = ['Toyota', 'Honda', 'Ford', 'BMW', 'Porsche'];
 
-car.power = function() {
-  console.log('Engine power: 200hp');
-};
+const carManufacturersString = carManufacturers.join(',');
+console.log(carManufacturersString);
 
-console.log(car);
-car.power();
+const carManufacturersArray = carManufacturersString.split(',');
+console.log(carManufacturersArray);
 
-const addFruits = (pears, apples) => pears + apples;
-console.log(addFruits(1,2));
+// Task 3
+const friends = ['Yura', 'Nazar', 'Andriyan', 'Denys', 'Pavlo'];
 
-const greetTerminal = (name) => {
-  if(name === 'Marko') {
-    console.log('Hello, ' + name);
-  } else {
-    console.log('There is no such name in the payment terminal');
-  }
+const greetings = friends.map(friend => 'Hello ' + friend);
+console.log(greetings);
+
+// Task 4
+const numbers = [0, 1, 2, 3, 4];
+const booleans = numbers.map(num => Boolean(num));
+console.log(booleans);
+
+// Task 5
+const numbersToSort = [1, 6, 7, 8, 3, 4, 5, 6];
+numbersToSort.sort((a, b) => b - a);
+console.log(numbersToSort);
+
+// Task 6
+const numbersToFilter = [1, 6, 7, 8, 3, 4, 5, 6];
+const filteredNumbers = numbersToFilter.filter(num => num > 3);
+console.log(filteredNumbers);
+
+// Task 7
+const findIndex = (arr, num) => arr.indexOf(num);
+
+const numbersToFindIndex = [1, 2, 3, 4, 5];
+console.log(findIndex(numbersToFindIndex, 3)); 
+
+// Task 8
+let a = 0;
+while (a < 10) {
+    console.log(a);
+    a++;
 }
-greetTerminal('Marko');
-greetTerminal('Max');
 
-const calculateType = (arg) => console.log('Argumet type: ' + typeof(arg));
-calculateType('test');
-calculateType(1);
-
+// Task 9
 const isPrime = (num) => {
   if (num <= 1) {
-    return false;
+      return false;
   }
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-        return false;
-    }
+      if (num % i === 0) {
+          return false;
+      }
   }
   return true;
 }
-console.log(isPrime(7));
-console.log(isPrime(6));
+
+let numberPrime = 2;
+while (numberPrime < 100) {
+  if (isPrime(numberPrime)) {
+      console.log(numberPrime);
+  }
+  numberPrime++;
+}
+
+// Task 10
+let numberOdd = 1;
+while (numberOdd < 100) {
+    console.log(numberOdd);
+    numberOdd += 2;
+}
+
+
