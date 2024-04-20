bc34b78e37aa3a69edbeae7303c8cfaca40d9202
myanovych <marko.yanovych.pz.2020@lpnu.ua>
2024-04-21 02:10:16 +0300

    Complete Exception handling task

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// Task 1
-const str1 = 'ahb acb aeb aeeb adcb axeb';
-const regex1 = /a.b/g;
-const matches1 = str1.match(regex1);
-console.log(matches1); 
+try {
+  console.log(a); 
+  let a = 3;
+} catch {
+  console.log('let must be declared before use'); 
+}
 
-// Task 2
-const str2 = '2 + 3 223 2223';
-const regex2 = /2 \+ 3/;
-const match2 = str2.match(regex2);
-console.log(match2[0]); 
-
-// Task 3
-const currentDate = new Date();
-const day = currentDate.getDate();
-const month = currentDate.getMonth() + 1; // Month is zero-based
-const year = currentDate.getFullYear();
-
-console.log('Day:', day);
-console.log('Month:', month);
-console.log('Year:', year);
+try {
+  const divisor = 0;
+  if (divisor === 0) {
+      throw new Error('cannot be divided by zero');
+  }
+  1 / divisor;
+} catch (err) {
+  console.error(err.message); 
+}
